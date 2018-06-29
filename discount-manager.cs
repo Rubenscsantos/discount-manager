c2285e60a2a748c5acb41f79cea5456d35803d75
Rubens Castro Santos <contato.rubenscsantos@gmail.com>
2018-06-29 02:58:38 -0300

    feat: Adds enumerable to identify AccountTypes

--- discount-manager.cs
+++ discount-manager.cs
@@ -1,48 +1,57 @@
 public class DiscountManager
+{
+  {
+    public enum AccountType
     {
-        public decimal CalculateTotalWithDiscounts(Customer customer)
-        {
-            decimal totalPurchaseValue = 0;
-
-            decimal customerPurchaseValue = customer.purchaseValue;
-
-            decimal discountAccordingToAmountOfYearsAsCustomer = CalculateDiscountAccordingToYearsAsCustomer(customer.yearsAsCustomer)
-
-            if (customer.accountType == 1)
-            {
-                totalPurchaseValue = customerPurchaseValue;
-            }
-            else if (customer.accountType == 2)
-            {
-                totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.1m, discountAccordingToAmountOfYearsAsCustomer)                
-            }
-            else if (customer.accountType == 3)
-            {
-                totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.7m, discountAccordingToAmountOfYearsAsCustomer)                
-            }
-            else if (customer.accountType == 4)
-            {
-                totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.5m, discountAccordingToAmountOfYearsAsCustomer)                
-            }
-
-            return totalPurchaseValue;
-        }
-        public calculateSubTotal(decimal purchaseValue, decimal discount, decimal discountAccordingToAmountOfYearsAsCustomer)
-        {
-            discountedValue = AddDiscount(purchaseValue, discount)
-            return discountedValue - discountAccordingToAmountOfYearsAsCustomer * discountedValue;
-        }
-        public CalculateDiscountAccordingToYearsAsCustomer(decimal yearsAsCustomer)
-        {
-            if (yearsAsCustomer > 5)
-            {
-                return 0.05m;
-            }
-            return yearsAsCustomer/100;
-        }
-        public AddDiscount(decimal value, decimal discount)
-        {
-            return value * discount;
-        }
+        NewCustomer = 1,
+        RecentCustomer = 2,
+        PlatinumCustomer = 3,
+        LoyalCustomer = 4
     }
+
+    public decimal CalculateTotalWithDiscounts(Customer customer)
+    {
+        decimal totalPurchaseValue = 0;
+
+        decimal customerPurchaseValue = customer.purchaseValue;
+
+        decimal discountAccordingToAmountOfYearsAsCustomer = CalculateDiscountAccordingToYearsAsCustomer(customer.yearsAsCustomer)
+
+        if (customer.accountType == AccountType.NewCustomer)
+        {
+            totalPurchaseValue = customerPurchaseValue;
+        }
+        else if (customer.accountType == AccountType.RecentCustomer)
+        {
+            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.1m, discountAccordingToAmountOfYearsAsCustomer)                
+        }
+        else if (customer.accountType == AccountType.PlatinumCustomer)
+        {
+            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.7m, discountAccordingToAmountOfYearsAsCustomer)                
+        }
+        else if (customer.accountType == AccountType.LoyalCustomer)
+        {
+            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.5m, discountAccordingToAmountOfYearsAsCustomer)                
+        }
+
+        return totalPurchaseValue;
+    }
+    public calculateSubTotal(decimal purchaseValue, decimal discount, decimal discountAccordingToAmountOfYearsAsCustomer)
+    {
+        discountedValue = AddDiscount(purchaseValue, discount)
+        return discountedValue - discountAccordingToAmountOfYearsAsCustomer * discountedValue;
+    }
+    public CalculateDiscountAccordingToYearsAsCustomer(decimal yearsAsCustomer)
+    {
+        if (yearsAsCustomer > 5)
+        {
+            return 0.05m;
+        }
+        return yearsAsCustomer/100;
+    }
+    public AddDiscount(decimal value, decimal discount)
+    {
+        return value * discount;
+    }
+  }
 }
