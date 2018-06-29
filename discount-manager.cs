50212b55b4014dda6fa209889a5ab6fae3737a39
Rubens Castro Santos <contato.rubenscsantos@gmail.com>
2018-06-29 04:28:53 -0300

    feat: Adds Strategy Pattern for Account Types

--- discount-manager.cs
+++ discount-manager.cs
@@ -1,57 +1,82 @@
-public class DiscountManager
+namespace Store
 {
-  {
-    public enum AccountType
+    public class DiscountManager
     {
-        NewCustomer = 1,
-        RecentCustomer = 2,
-        PlatinumCustomer = 3,
-        LoyalCustomer = 4
-    }
-
-    public decimal CalculateTotalWithDiscounts(Customer customer)
-    {
-        decimal totalPurchaseValue = 0;
-
-        decimal customerPurchaseValue = customer.purchaseValue;
-
-        decimal discountAccordingToAmountOfYearsAsCustomer = CalculateDiscountAccordingToYearsAsCustomer(customer.yearsAsCustomer)
-
-        if (customer.accountType == AccountType.NewCustomer)
+        public enum AccountType
         {
-            totalPurchaseValue = customerPurchaseValue;
-        }
-        else if (customer.accountType == AccountType.RecentCustomer)
-        {
-            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.1m, discountAccordingToAmountOfYearsAsCustomer)                
-        }
-        else if (customer.accountType == AccountType.PlatinumCustomer)
-        {
-            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.7m, discountAccordingToAmountOfYearsAsCustomer)                
-        }
-        else if (customer.accountType == AccountType.LoyalCustomer)
-        {
-            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, 0.5m, discountAccordingToAmountOfYearsAsCustomer)                
+            NewCustomer = 1,
+            RecentCustomer = 2,
+            PlatinumCustomer = 3,
+            LoyalCustomer = 4
         }
 
-        return totalPurchaseValue;
-    }
-    public calculateSubTotal(decimal purchaseValue, decimal discount, decimal discountAccordingToAmountOfYearsAsCustomer)
-    {
-        discountedValue = AddDiscount(purchaseValue, discount)
-        return discountedValue - discountAccordingToAmountOfYearsAsCustomer * discountedValue;
-    }
-    public CalculateDiscountAccordingToYearsAsCustomer(decimal yearsAsCustomer)
-    {
-        if (yearsAsCustomer > 5)
+        private static IAccountTypeDiscountStrategy GetAccountTypeImplementation(AccountType accountType)
         {
-            return 0.05m;
+            IAccountTypeDiscountStrategy accountTypeDiscountStrategy = null;
+
+            switch (accountType)
+            {
+                case AccountType.NewCustomer:
+                    accountTypeDiscountStrategy = 0m;              
+                    break;
+                case AccountType.RecentCustomer:
+                    accountTypeDiscountStrategy = 0.1m;
+                    break;
+                case AccountType.PlatinumCustomer:
+                    accountTypeDiscountStrategy = 0.7m;
+                    break;
+                case AccountType.LoyalCustomer:
+                accountTypeDiscountStrategy = 0.5m;
+                    break;
+                default:
+                    break;
+            }
+            return accountTypeDiscountStrategy;
         }
-        return yearsAsCustomer/100;
+
+        public decimal CalculateTotalWithDiscounts(Customer customer, decimal purchaseValue)
+        {
+            decimal totalPurchaseValue = 0;
+
+            decimal customerPurchaseValue = purchaseValue;
+
+            decimal discountAccordingToAmountOfYearsAsCustomer = CalculateDiscountAccordingToYearsAsCustomer(customer.yearsAsCustomer);
+
+            AccountType customerAccountType = customer.accountType
+
+            discountToBeApplied = GetAccountTypeImplementation(customerAccountType);
+            totalPurchaseValue = calculateSubTotal(customerPurchaseValue, discountToBeApplied, discountAccordingToAmountOfYearsAsCustomer);
+
+            return totalPurchaseValue;
+        }
+        public decimal calculateSubTotal(decimal purchaseValue, decimal discount, decimal discountAccordingToAmountOfYearsAsCustomer)
+        {
+            if discount == 0
+            {
+                return purchaseValue;
+            }
+            discountedValue = AddDiscount(purchaseValue, discount);
+            return discountedValue - discountAccordingToAmountOfYearsAsCustomer * discountedValue;
+        }
+        public decimal CalculateDiscountAccordingToYearsAsCustomer(decimal yearsAsCustomer)
+        {
+            if (yearsAsCustomer > 5)
+            {
+                return 0.05m;
+            }
+            return yearsAsCustomer/100;
+        }
+        public decimal AddDiscount(decimal value, decimal discount)
+        {
+            return value * discount;
+        }
     }
-    public AddDiscount(decimal value, decimal discount)
+
+    public class Customer
     {
-        return value * discount;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime YearsAsCustomer { get; set; }
+        public int AccountType { get; set; }
     }
-  }
 }
